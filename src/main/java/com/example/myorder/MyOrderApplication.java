09c6df4364277aa0bc2c764e337789c522aa51a8
jubileu99 <victor_luizbernardes5@outlook.com>
2020-02-10 17:34:07 -0200

    1st commit

---
+++ src/main/java/com/example/myorder/MyOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.myorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOrderApplication.class, args);
+	}
+
+}
